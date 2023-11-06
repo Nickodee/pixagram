f41cabbb7121740b06afd0151e18ef7b34f11744
Nickodee <nicodemusmuholo@gmail.com>
2023-11-06 17:10:28 +0300

    creating and registering user model

--- backend/pixagramapi/authentication/admin.py
+++ backend/pixagramapi/authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
+
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'fisrt_name', 'last_name', 'email']
+admin.site.register(User, UserAdmin)
